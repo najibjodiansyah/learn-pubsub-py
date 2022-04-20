3b7b4bc25886dedd007ba9b2e33532dd8a11345a
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-04-21 02:22:31 +0700

    fix credential path

--- subscriber.py
+++ subscriber.py
@@ -1,7 +1,8 @@
 import os
 from google.cloud import pubsub_v1
+from concurrent.future import TimeoutError
 
-credential_path = '/Users/najibjodiansyah/Documents/pubsubpy/still-sensor-private-key.still-sensor-private-key.json'
+credential_path = '/Users/najibjodiansyah/Documents/pubsubpy/still-sensor-private-key.json'
 os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credential_path
 
 
