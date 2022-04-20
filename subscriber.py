2025b3e5b5cc1c9be09b2e7519c83ea978eaa8ac
najibjodiansyah <najibjodiansyah11@gmail.com>
2022-04-21 01:54:01 +0700

    add subscriber save

--- subscriber.py
+++ subscriber.py
@@ -9,3 +9,18 @@ timeout = 10.0
 
 subscriber = pubsub_v1.SubscriberClient()
 subscription_path = 'projects/still-sensor-339610/subscriptions/Testing-PubSub-Py-sub'
+
+def callback(message):
+    print('Recevied message:'+{message})
+    print('data: '+ {message.data})
+    message.ack()
+
+streaming_pull_future = subscriber.subscribe(subscription_path, callback=callback)
+print('listening for message on' + subscription_path)
+
+with subscriber:
+    try:
+        streaming_pull_future.result()
+    except TimeoutError:
+        streaming_pull_future.cancel()
+        streaming_pull_future.result()
